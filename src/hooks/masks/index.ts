ac7e5b24c016b9492da3ed423a5c6c1bc7a31087
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-03-14 17:40:56 -0300

    root máscaras

---
+++ src/hooks/masks/index.ts
@@ -0,0 +1,9 @@
+import { cep } from './cep';
+import { cnpj } from './cnpj';
+import { tel } from './tel';
+import { cpf } from './cpf';
+import { currency } from './currency';
+import { document } from './document';
+import { percent } from './percent';
+
+export { cep, cnpj, tel, cpf, currency, document, percent };
