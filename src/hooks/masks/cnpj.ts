dc134bfdc5b6aaf18d6693a42335555a84113e3a
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-03-14 17:37:26 -0300

    aplicar máscara de cnpj

---
+++ src/hooks/masks/cnpj.ts
@@ -0,0 +1,12 @@
+const cnpj = (value: string): string => {
+  value = value.replace(/\D/g, '');
+
+  return value
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export { cnpj };
