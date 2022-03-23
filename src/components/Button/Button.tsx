d1819d29c0ff709e48698c90d672189a19fe25dd
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-03-23 19:00:54 -0300

    adicionar estados de desabilitado e carregando dos botões

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiLoader } from 'react-icons/fi';
 
-import { ButtonStyle } from './ButtonStyles';
+import { ButtonStyle, Loader } from './ButtonStyles';
 
 export interface IButtonProps
   extends React.DetailedHTMLProps<
@@ -9,17 +10,32 @@ export interface IButtonProps
   > {
   isDisabled?: boolean;
   variant?: 'primary' | 'secondary' | 'info' | 'warning' | 'danger';
+  isLoading?: boolean;
+  loadingText?: string;
 }
 
 export const Button: React.FunctionComponent<IButtonProps> = ({
   children,
   variant = 'primary',
   isDisabled = false,
+  isLoading = false,
+  loadingText = 'Carregando...',
   ...props
 }) => {
   return (
-    <button disabled={isDisabled} className={ButtonStyle({ variant })} {...props}>
-      {children}
+    <button
+      {...props}
+      disabled={isDisabled}
+      className={ButtonStyle({ variant, disabled: isDisabled || isLoading })}
+    >
+      {isLoading && (
+        <div className={Loader()}>
+          <FiLoader />
+          {loadingText}
+        </div>
+      )}
+
+      {!isLoading && children}
     </button>
   );
 };
--- src/components/Button/ButtonStyles.ts
+++ src/components/Button/ButtonStyles.ts
@@ -1,4 +1,10 @@
+import { keyframes } from '@stitches/core';
 import { css } from '../../theme';
+
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' }
+});
 
 export const ButtonStyle = css('button', {
   all: 'unset',
@@ -9,18 +15,14 @@ export const ButtonStyle = css('button', {
   fontWeight: '$bold',
   transition: 'filter .2s, box-shadow .2s, opacity .2s',
   cursor: 'pointer',
+  userSelect: 'none',
 
-  '&:hover:not([disabled])': {
+  '&:hover:not([disabled]):not(:active)': {
     filter: 'brightness(0.9)'
   },
 
   '&:active': {
-    filter: 'brightness(0.6)'
-  },
-
-  '&[disabled]': {
-    cursor: 'not-allowed',
-    opacity: 0.5
+    filter: 'brightness(0.8)'
   },
 
   '&:focus': {
@@ -50,10 +52,27 @@ export const ButtonStyle = css('button', {
         backgroundColor: '$red',
         color: 'White'
       }
+    },
+    disabled: {
+      true: {
+        pointerEvents: 'none',
+        opacity: 0.6
+      }
     }
   },
 
   defaultVariants: {
     variant: 'primary'
+  }
+});
+
+export const Loader = css({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  svg: {
+    marginRight: '$4',
+    animation: `${spin} 1s infinite linear`
   }
 });
