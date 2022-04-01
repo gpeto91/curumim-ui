51d45486c986c8960544336d5f38f6b86f8bac7d
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-04-01 17:47:04 -0300

    atualizar to-do's

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 export * from './components';
 
 // TODO componente multiselect
-// TODO componente de busca
 // TODO componente de paginação
-// TODO componente de checkbox
-// TODO componente de radio button
-// TODO componente switch
 // TODO componente autocomplete com API
+// TODO componente datepicker
