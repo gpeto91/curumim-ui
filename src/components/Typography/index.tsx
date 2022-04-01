c77168495dc5ec80bdc1e539819d1fa47ec821fc
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-04-01 17:49:04 -0300

    adicionar to-do's de tipografia

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -2,3 +2,7 @@ export { Caption } from './Caption';
 export { Subtitle } from './Subtitle';
 export { Title } from './Title';
 export { Text } from './Text';
+
+// TODO remover margin dos elementos
+// TODO adicionar prop uppercase do título e subtítulo
+// TODO adicionar prop de alterar cor do texto
