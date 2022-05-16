f87e8167864192505b3ac601cea8faa0f82d2cf8
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-05-16 12:21:25 -0300

    adicionar storybook do Input

--- src/stories/Componentes/Input.stories.tsx
+++ src/stories/Componentes/Input.stories.tsx
@@ -5,12 +5,37 @@ import { Input } from '../../components/Input';
 
 export default {
   title: 'Componentes/Input',
-  component: Input
+  component: Input,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          fontFamily: 'Roboto',
+          backgroundColor: '#efefef',
+          maxWidth: 400,
+          margin: '0 auto',
+          padding: 20
+        }}
+      >
+        {Story()}
+      </div>
+    )
+  ],
+  parameters: {
+    docs: {
+      source: {
+        excludeDecorators: true
+      }
+    }
+  }
 } as ComponentMeta<typeof Input>;
 
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+const Template: ComponentStory<typeof Input> = (args) => {
+  return <Input {...args} />;
+};
 
 export const Primary = Template.bind({});
+
 Primary.args = {
   id: 'my-id'
 };
